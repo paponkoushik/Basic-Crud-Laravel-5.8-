3717d2bb147e4aabb556b484d89a8b4a12a67069
paponkoushik <paponkoushik@gmail.com>
2019-07-20 18:58:44 +0600

    design has been changed

--- CRUD/resources/views/createStudent.blade.php
+++ CRUD/resources/views/createStudent.blade.php
@@ -1,1 +1,9 @@
-<?php
+@extends('layouts.app')
+@section('content')
+    <div class="card">
+        <div class="card-header">Add New Student</div>
+        <div class="card-body">
+
+        </div>
+    </div>
+@endsection
