c548b7d6c5dbaee7ac8e498cb807270b3ed211be
paponkoushik <paponkoushik@gmail.com>
2019-07-20 19:47:42 +0600

    type has been changed

--- CRUD/resources/views/createStudent.blade.php
+++ CRUD/resources/views/createStudent.blade.php
@@ -3,28 +3,29 @@
     <div class="card">
         <div class="card-header">Add New Student</div>
         <div class="card-body">
-            <form>
+            <form action="{{ route('store') }}" method="POST">
+                {{ csrf_field() }}
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="inputEmail4">First Name</label>
-                        <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                        <input type="text" class="form-control" id="inputEmail4" placeholder="First Name">
                     </div>
                     <div class="form-group col-md-6">
-                        <label for="inputPassword4">Password</label>
-                        <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                        <label for="inputPassword4">Last Name</label>
+                        <input type="text" class="form-control" id="inputPassword4" placeholder="Last Name">
                     </div>
                 </div>
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="inputEmail4">Email</label>
-                        <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                        <input type="email" class="form-control" id="inputEmail4" placeholder="xyz@email.com">
                     </div>
                     <div class="form-group col-md-6">
-                        <label for="inputPassword4">Password</label>
-                        <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                        <label for="inputPassword4">Phone Number</label>
+                        <input type="text" class="form-control" id="inputPassword4" placeholder="01XXXXXXXXX">
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Sign in</button>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
     </div>
