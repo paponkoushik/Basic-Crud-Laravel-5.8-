9641098cc8ef8035a45b5eaff7ddb88f2fc205e8
paponkoushik <paponkoushik@gmail.com>
2019-07-20 19:46:52 +0600

    A new route create for add data

--- CRUD/routes/web.php
+++ CRUD/routes/web.php
@@ -13,3 +13,4 @@
 
 Route::get('/', 'StudentController@index')->name('home');
 Route::get('create', 'StudentController@create')->name('create');
+Route::post('create', 'StudentController@store')->name('store');
