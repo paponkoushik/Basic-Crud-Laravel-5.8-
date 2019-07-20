fffb56c38aabbe24966c15a190da520cfb203d88
paponkoushik <paponkoushik@gmail.com>
2019-07-20 19:29:44 +0600

    A design has been created

--- CRUD/resources/views/createStudent.blade.php
+++ CRUD/resources/views/createStudent.blade.php
@@ -3,7 +3,29 @@
     <div class="card">
         <div class="card-header">Add New Student</div>
         <div class="card-body">
-
+            <form>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="inputEmail4">First Name</label>
+                        <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="inputPassword4">Password</label>
+                        <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="inputEmail4">Email</label>
+                        <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="inputPassword4">Password</label>
+                        <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary">Sign in</button>
+            </form>
         </div>
     </div>
 @endsection
