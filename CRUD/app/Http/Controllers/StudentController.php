9296ae801efa75aa7abf18f6487bc85ab5d9e4cc
paponkoushik <paponkoushik@gmail.com>
2019-07-21 18:37:29 +0600

    data insert complete

--- CRUD/app/Http/Controllers/StudentController.php
+++ CRUD/app/Http/Controllers/StudentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\students;
 
 class StudentController extends Controller
 {
@@ -16,8 +17,21 @@ class StudentController extends Controller
         return view('createStudent');
     }
 
-    public function store(Request $request, Response $response)
+    public function store(Request $request)
     {
-        return $request->all();
+        $this->validate($request, [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required',
+            'phone' => 'required'
+        ]);
+
+        $student = new students;
+        $student->first_name = $request->first_name;
+        $student->last_name = $request->last_name;
+        $student->email = $request->email;
+        $student->phone = $request->phone;
+        $student->save();
+        return redirect(route('home'))->with('success', 'Student Has Been Added Successfully');
     }
 }
