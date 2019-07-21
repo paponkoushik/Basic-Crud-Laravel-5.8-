45e5f52c913a914b3441da5479be5b90e4f6709b
paponkoushik <paponkoushik@gmail.com>
2019-07-21 18:38:32 +0600

    Alert message added.

--- CRUD/resources/views/welcome.blade.php
+++ CRUD/resources/views/welcome.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.app')
 @section('content')
-    <h1>Hellow Laravel</h1>
+    <div class="container">
+        @if (session('success'))
+            <div class="alert alert-success alert-dismissible">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <strong>Success!</strong> {{ session('success') }}.
+            </div>
+        @endif
+    </div>
 @endsection
